35d2809a16ed676cb9506512ee18360360904058
ralitsavoronevska <ralicavoronevska@gmail.com>
2023-12-30 16:49:41 +0200

    Changed the API key

--- 18-forkify/starter/src/js/config.js
+++ 18-forkify/starter/src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '2ff3277a-983e-4763-8462-66593c012f42';
+export const KEY = '40fb2597-e44e-426f-b271-613793e58e68';
 export const MODAL_CLOSE_SEC = 2500;
